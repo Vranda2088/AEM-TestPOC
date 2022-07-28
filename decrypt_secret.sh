a569d7136e7f5f613dd321d14128605378977f13
Vranda2088 <62051031+Vranda2088@users.noreply.github.com>
2022-07-28 16:22:07 +0530

    Create decrypt_secret.sh

---
+++ decrypt_secret.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Decrypt the file
+mkdir $HOME/secrets
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" --output $HOME/secrets/config.json config.json.gpg
